10b8651a721dea603b44bd0c10df2a269c71b4e8
Alberto <astriedinger@gmail.com>
2014-07-28 00:42:54 +0200

    Correcting minor typos in the ## comments of the R-file

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Dear all, the next functions are exactly as the example provided with a minor modification.
-## This was the simplest way I found to solve the assigment. The two functions below will make a matrix, and cache its inverse
-## The invers is calculated, and if the inverse is needed again, the value will be read and given from the cache =)
+## This was the simplest way I found to solve the assignment. The two functions below will make a matrix, and cache its inverse
+## The inverse is calculated, and if the inverse is needed again, the value will be read and given from the cache =)
 
 
 ## Hence, the function makeCacheMatrix creates special matrix that can cache its inverse
@@ -19,9 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This second function (cacheSolve) will compute the inverse of the previous special matrix 
-## (provided by makeCacheMatrix)and return it to the user.
+## (provided by makeCacheMatrix) and return it to the user.
 ## In the case the inverse has already been calculated, the function will use the value 
-# stored in the cahe and return this value to the user
+## stored in the cahe and return this value to the user
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
